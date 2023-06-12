2d31be1414b47c4b019fdae9db4d74548301848c
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-06-12 06:50:41 +0600

    [DONE] fix some previous error

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
-import usersRouter from './app/modules/users/users.route'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import { UserRoutes } from './app/modules/users/user.route'
 const app: Application = express()
 
 app.use(cors())
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // application routes
-app.use('/api/v1/users', usersRouter)
+app.use('/api/v1/users', UserRoutes.router)
 
 // testing
 app.get('/', (req: Request, res: Response) => {
