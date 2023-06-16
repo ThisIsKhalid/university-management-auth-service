e286f2800ecbcb524f719650b95d69b3998de23b
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-06-16 22:58:32 +0600

    added academic service

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express, { Application } from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route';
 import { UserRoutes } from './app/modules/user/user.route';
 const app: Application = express();
 
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // application routes
 app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
 
 // testing
 // app.get('/', (req: Request, res: Response) => {
