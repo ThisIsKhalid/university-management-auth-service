d7a732a23bd48da858478e4253528d7a570c0dff
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-06-18 07:16:34 +0600

    created and optimized routes, controller

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
 import cors from 'cors';
 import express, { Application } from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route';
-import { UserRoutes } from './app/modules/user/user.route';
+import routes from './app/routes';
 const app: Application = express();
 
 app.use(cors());
@@ -12,8 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // application routes
-app.use('/api/v1/users', UserRoutes);
-app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
+app.use('/api/v1/', routes);
 
 // testing
 // app.get('/', (req: Request, res: Response) => {
--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -1,4 +1,6 @@
+import httpStatus from 'http-status';
 import { Schema, model } from 'mongoose';
+import ApiError from '../../../errors/ApiErrors';
 import {
   academicSemesterCodes,
   academicSemesterMonths,
@@ -40,6 +42,21 @@ const academicSemesterSchema = new Schema<IAcademicSemester>(
     timestamps: true, // createdAt, updatedAt
   }
 );
+
+// pre hook always model er upore use korte hobe
+academicSemesterSchema.pre('save', async function (next) {
+  const isExist = await AcademicSemester.findOne({
+    title: this.title,
+    year: this.year,
+  });
+  if (isExist) {
+    throw new ApiError(
+      httpStatus.CONFLICT,
+      'Academic semester is already exist !'
+    );
+  }
+  next(); // this next is mongoose function
+});
 
 export const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
   'AcademicSemester',
